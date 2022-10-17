a2127fdf899f93b64a5838d06b8bd4be1be3af31
Tomas <guzzo.tomas00@gmail.com>
2022-10-17 10:44:42 -0300

    subo bonus

--- Entregable/bonus/script.js
+++ Entregable/bonus/script.js
@@ -1,3 +1,9 @@
+
+let randomWord = document.getElementById("randomWord");
+const text = document.querySelector("input");
+let timeSpan = document.getElementById("timeSpan");
+let Score = document.getElementById("score");
+
 const words = [
     'californication',
     'plataforma5',
@@ -20,5 +26,62 @@ const words = [
     'chili'
 ];
 
+let palabraAleatoria 
+let time = 10;
+let score = 0;
 
-  
+function randomWords() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+function addToDOM() {
+    palabraAleatoria = randomWords();
+    randomWord.innerText = palabraAleatoria;
+}
+
+addToDOM();
+
+
+let palabraIngresada
+
+text.addEventListener("input", compararPalabras);
+
+function compararPalabras(e) {
+    //console.log(e.target.value);
+    palabraIngresada = e.target.value;
+    if (palabraIngresada === palabraAleatoria) {
+        console.log("correcto");
+        time += 3;
+        e.target.value = "";
+        addToDOM();
+        updateScore();
+    }
+}
+
+let interval = setInterval(actualizarTiempo, 1000);
+
+function actualizarTiempo(){
+    time--; 
+    timeSpan.innerText = time + "s";
+    if (time === 0) {
+        clearInterval(interval);
+        gameOver();
+    }
+}
+
+function updateScore() {
+    score++;
+    Score.innerText = score;
+}
+
+
+
+function gameOver() {
+    main.innerHTML = `<h1></h1>`;
+    endgamecontainer.innerHTML = `
+    <h1>GAME OVER</h1>
+    <p id="puntaje">Puntaje final ${score}</p>
+    <button class="botonGameOver"onclick="location.reload()">Volver a jugar</button>
+    `;
+    endGameContainer.style.display = "flex";
+}
